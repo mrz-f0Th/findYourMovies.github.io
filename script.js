6428c894ce22651929ffe218f287c529e871a9bc
mrz-f0Th <rakazzakari22@gmail.com>
2021-03-14 07:29:14 -0700

    remove jQuery replace with fetch

--- script.js
+++ script.js
@@ -1,32 +1,67 @@
-$('.search-button').on('click', ()=> {
-    $.ajax({
-        url: 'https://www.omdbapi.com/?apikey=415a8604&s=' + $('.input-keyword').val(),
-        success: results => {
-            $('.input-keyword').val('');
-            const movie = results.Search;
-            let cards = '';
-            movie.forEach(mv => {
-                cards += tampilFilm(mv);
-            });
-            $('.movie-container').html(cards);
+// Using jQuery
+
+// $('.search-button').on('click', ()=> {
+//     $.ajax({
+//         url: 'https://www.omdbapi.com/?apikey=415a8604&s=' + $('.input-keyword').val(),
+//         success: results => {
+//             $('.input-keyword').val('');
+//             const movie = results.Search;
+//             let cards = '';
+//             movie.forEach(mv => {
+//                 cards += tampilFilm(mv);
+//             });
+//             $('.movie-container').html(cards);
     
-            $('.modal-detail-button').on('click', function() {
-                $.ajax({
-                    url: 'https://www.omdbapi.com/?apikey=415a8604&i=' + $(this).data('imdbid'),
-                    success: md => {
-                        const modal = tampilModal(md);
-                        $('.modal-body').html(modal);
-                    },
-                    error: e => {
-                        console.log(e.responseText);
-                    }
+//             $('.modal-detail-button').on('click', function() {
+//                 $.ajax({
+//                     url: 'https://www.omdbapi.com/?apikey=415a8604&i=' + $(this).data('imdbid'),
+//                     success: md => {
+//                         const modal = tampilModal(md);
+//                         $('.modal-body').html(modal);
+//                     },
+//                     error: e => {
+//                         console.log(e.responseText);
+//                     }
+//                 })
+//             })
+//         },
+//         error: e => {
+//             console.log(e.responseText);
+//         }
+//     })
+// })
+
+
+
+// Using Fetch 
+
+const btnTrigger = document.querySelector('.search-button');
+btnTrigger.addEventListener('click', ()=> {
+
+    const inputKeyword = document.querySelector('.input-keyword');
+    fetch('https://www.omdbapi.com/?apikey=415a8604&s=' + inputKeyword.value)
+        .then(response => response.json())
+        .then(response => {
+
+            const movies = response.Search;
+            let card = '';
+            movies.forEach( mv=> card += tampilFilm(mv) );
+            const movieContainer = document.querySelector('.movie-container')
+            movieContainer.innerHTML = card;
+
+            const mdButton = document.querySelectorAll('.modal-detail-button');
+            mdButton.forEach(btn=> {
+                btn.addEventListener('click', function() {
+                    fetch('https://www.omdbapi.com/?apikey=415a8604&i=' + this.dataset.imdbid)
+                        .then(response=> response.json())
+                        .then(mb=> {
+                            const viewModal = tampilModal(mb);
+                            const modalBody = document.querySelector('.modal-body');
+                            modalBody.innerHTML = viewModal;
+                        })
                 })
             })
-        },
-        error: e => {
-            console.log(e.responseText);
-        }
-    })
+        })
 })
 
 
@@ -53,12 +88,12 @@ function tampilModal(md) {
                     <div class="col-md">
                         <ul class="list-group">
                             <li class="list-group-item">
-                                <h4>Avengers (2022)</h4>
+                                <h4>${md.Title} ${md.Year}</h4>
                             </li>
-                            <li class="list-group-item"><strong>Director : </strong>${md.Title} ${md.Year}
+                            <li class="list-group-item"><strong>Director : </strong>${md.Director}
                             </li>
-                            <li class="list-group-item"><strong>Actors : </strong>${md.Director}</li>
-                            <li class="list-group-item"><strong>Writer : </strong>${md.Actors}</li>
+                            <li class="list-group-item"><strong>Actors : </strong>${md.Actors}</li>
+                            <li class="list-group-item"><strong>Writer : </strong>${md.Writer}</li>
                             <li class="list-group-item"><strong>Plot : </strong> <br>${md.Plot} mempunyai
                                 kekuatan
                                 untuk menguasai Bumi</li>
